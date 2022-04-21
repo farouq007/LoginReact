ec270165ead4d1e42aecc8cad8502e7f00523f7b
Farouq Ogunyele <TAJ424@tajbank.com>
2022-04-21 13:42:34 +0100

    Add project files.

---
+++ LoginReact/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using LoginReact.Data;
+using LoginReact.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginReact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<string>> Login(User user)
+        {
+            _context.UsersLogin.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("Welcome");
+        }
+    }
+}
---
+++ LoginReact/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginReact.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginReact.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> UsersLogin { get; set; }
+    }
+}
